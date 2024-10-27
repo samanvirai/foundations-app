a67cd587d8532d60a4a196e007f9bed437eb4d3c
Maneesha Venigalla <venigallamaneesha24@gmail.com>
2024-10-27 00:38:56 -0400

    Removed unused apollo call

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
-
-// Define the GraphQL query
-const HELLO_QUERY = gql`
-  query GetHello {
-    hello
-  }
-`;
+import React from "react";
 
 const App: React.FC = () => {
-  // Use Apollo's useQuery hook to make a query request
-  const { loading, error, data } = useQuery(HELLO_QUERY);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>GraphQL Apollo Client Example</h1>
-        <p>{data.hello}</p> {/* Display the hello response from GraphQL API */}
+        <h1>Welcome to Foundations App!</h1>
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { ApolloProvider } from '@apollo/client';
-import client from './ApolloClient';
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ApolloProvider } from "@apollo/client";
+import client from "./ApolloClient";
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
